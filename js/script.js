521c8869ffe269565c51356868c8196d16f59951
mizbatista <contatopess.batistadev@hotmail.com>
2026-06-27 13:17:55 -0300

    feat: footer + botão voltar ao topo acompanhado de estilizacao extras

--- js/script.js
+++ js/script.js
@@ -148,3 +148,35 @@ window.addEventListener("scroll", () => {
     });
 
 });
+
+// ==========================================
+// BOTÃO VOLTAR AO TOPO
+// ==========================================
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+
+        backToTop.classList.add("show");
+
+    } else {
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+});
